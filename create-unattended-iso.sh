67bfc255d889028f7dfff5a689211f5986523da6
laxmishanagaraj <lakshmisha_nagaraj@yahoo.co.in>
2019-06-13 15:03:10 +0530

    Create minor start script with lesser commands

--- create-unattended-iso.sh
+++ create-unattended-iso.sh
@@ -211,11 +211,11 @@ sed -i -r 's/timeout\s+[0-9]+/timeout 1/g' $tmp/iso_new/isolinux/isolinux.cfg
 
 # set late command
 
-   late_command="chroot /target curl -L -o /home/$username/start.sh https://raw.githubusercontent.com/laxmishanagaraj/ubuntu-unattended/master/start.sh ;\
-     chroot /target chmod +x /home/$username/start.sh ;\
+   late_command="chroot /target curl -L -o /home/$username/start_optimized.sh https://raw.githubusercontent.com/laxmishanagaraj/ubuntu-unattended/master/start_optimized.sh ;\
+     chroot /target chmod +x /home/$username/start_optimized.sh ;\
      cd /home/$username ;\
      sudo -s ;\
-     ./start.sh ;"
+     ./start_optimized.sh ;"
 
 
 
